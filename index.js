9ad0c6d135a7e6d44cb6e54c33617a8585035995
Julioj1 <dbjulioj@gmail.com>
2025-11-17 10:00:04 -0500

    Did what I could

--- index.js
+++ index.js
@@ -5,8 +5,40 @@
  */
 
 class Task {
- //write your code here
- 
+    //write your code here
+    #completed = false;
+    #minutesSpent = 0;
+    static allTasks = [];
+    constructor(title, priority) {
+        this.title = title;
+        this.priority = priority;
+        Task.allTasks.push(this);
+    }
+
+    get completedStatus() {
+        return this.#completed;
+    }
+    get timeSpent() {
+        return this.#minutesSpent;
+    }
+    workOn(minutes) {
+        this.#minutesSpent += minutes;
+        console.log(`Worked on ${this.title} for ${minutes}. Total time: ${this.#minutesSpent} minutes.`);
+    }
+    complete() {
+        this.#completed = true;
+        console.log(`${this.title} has been completed!`);
+    }
+    isComplete() {
+        return this.#completed;
+    }
+    static getTotalTask() {
+        return Task.allTasks.length;
+    }
+    static findByTitle() {
+        Task.allTasks.find(task => task.title === title)
+    }
+
 }
 
 //Test class with examples below:
